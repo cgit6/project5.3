d3149e41f2b9820c57003ae0e5a50fce79d704e3
cgit6 <cgit67534@gmail.com>
2022-08-27 00:57:30 +0800

    b commit

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ dotenv.config();
 // 把auth-route接進來
 const authRoute = require("./routes/auth-route");
 
-const profileRoute = require("./routes/profile-route")
+const profileRoute = require("./routes/profile-route");
 // 為什麼不用加const變數?
 require("./config/passport");
 // const cookieSession = require("cookie-session");
 
 const session = require("express-session");
-const flash = require("connect-flash")
+const flash = require("connect-flash");
 
 const passport = require("passport");
+const port = process.env.POST || 8080;
 
 // mongodb basic setting
 mongoose
@@ -41,11 +42,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 先設定session
-app.use(session({
-  secret: process.env.SECRET,
-  resave:false,
-  saveUninitialized:true,
-})
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
 );
 
 // 第二步設定passport
@@ -54,9 +56,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // 最後才是flash
-// flash 
+// flash
 app.use(flash());
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   // passport 專用
@@ -65,20 +67,19 @@ app.use((req,res,next) => {
 });
 
 // 最後才是route
-// 當node.js接收到的任何request 會經過middleware 會去檢查有沒有 /auth 
-// 如果有就會進入authRoute去判斷 
+// 當node.js接收到的任何request 會經過middleware 會去檢查有沒有 /auth
+// 如果有就會進入authRoute去判斷
 // 選擇要執行 /login 還是 /google
 
 // 就是 auth/...後面再去接就是源自這裡
 app.use("/auth", authRoute);
 app.use("/profile", profileRoute);
 
-
 // route
 app.get("/", (req, res) => {
-  res.render("index",{ user:req.user});
+  res.render("index", { user: req.user });
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log("serrver is runnong on port 8080");
 });

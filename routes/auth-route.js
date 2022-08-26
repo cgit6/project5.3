10a416392773537fd3691fff7a7148e355b9dca2
cgit6 <cgit67534@gmail.com>
2022-08-27 02:01:49 +0800

    c commit

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,70 +1,64 @@
 const router = require("express").Router();
 const passport = require("passport");
 const bcrypt = require("bcrypt");
-const User = require("../models/user-model")
-
+const User = require("../models/user-model");
 
 // 登入
-router.get("/login", (req,res) => {
-  res.render("login" , { user:req.user });
+router.get("/login", (req, res) => {
+  res.render("login", { user: req.user });
 });
 
 // 本地註冊
-router.get("/signup" , (req,res) => {
-  res.render("signup", { user:req.user });
+router.get("/signup", (req, res) => {
+  res.render("signup", { user: req.user });
 });
 
 // 登出
-router.get("/logout" , (req,res) => {
+router.get("/logout", (req, res) => {
   req.logOut();
   res.redirect("/");
-
 });
 
 router.post(
   "/login",
   passport.authenticate("local", {
-  
-  failureRedirect: '/auth/login',
-  failureFlash: "wrong email or password",
-}),
-(req,res) => {
-  if (req.session.returnTo) {
-    let newPath = req.session.returnTo;
-    req.session.returnTo = "";
-    res.redirect(newPath);// profile/path
-  }else {
-    res.redirect("/profile");
-  }
+    failureRedirect: "/auth/login",
+    failureFlash: "wrong email or password",
+  }),
+  (req, res) => {
+    if (req.session.returnTo) {
+      let newPath = req.session.returnTo;
+      req.session.returnTo = "";
+      res.redirect(newPath); // profile/path
+    } else {
+      res.redirect("/profile");
+    }
   }
 );
 
-router.post("/signup" , async(req,res) => {
-
+router.post("/signup", async (req, res) => {
   console.log(req.body);
   // res.send("Thank for posting.")
-  let {name , email ,password} = req.body;
+  let { name, email, password } = req.body;
   // check if the data is already in db
-  const emailExist = await User.findOne({email});
-  if(emailExist) {
-    req.flash("error_msg" , "Email has already been registered");
+  const emailExist = await User.findOne({ email });
+  if (emailExist) {
+    req.flash("error_msg", "Email has already been registered");
     // redirect means?
     res.redirect("/auth/signup");
   }
 
-
   // 加密
-  const hash = await bcrypt.hash(password,10);
+  const hash = await bcrypt.hash(password, 10);
   password = hash;
-  let newUser = new User({name,email,password});
+  let newUser = new User({ name, email, password });
   try {
     await newUser.save();
-    req.flash("success_msg" , "Registration succeeds. you can login now.");
+    req.flash("success_msg", "Registration succeeds. you can login now.");
     res.redirect("/auth/login");
-  } 
-  catch (err) {
-    req.flash("error_msg" , err.errors.name.properties.message);
-    res.redirect("/auth/signup")
+  } catch (err) {
+    req.flash("error_msg", err.errors.name.properties.message);
+    res.redirect("/auth/signup");
   }
 });
 
@@ -73,7 +67,7 @@ router.get(
   "/google",
   // 要用passport 對google 做驗證
   passport.authenticate("google", {
-    // 什麼是 scope 
+    // 什麼是 scope
     // 什麼是 profile? 就是使用者在google的全部資料
     // 什麼是 email 就是使用者在google的email資料
 
@@ -81,14 +75,14 @@ router.get(
     scope: ["profile", "email"],
     prompt: "select_account",
   })
-  );
-  
-router.get("/google/redirect", passport.authenticate("google"), (req,res) => {
+);
+
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   if (req.session.returnTo) {
     let newPath = req.session.returnTo;
     req.session.returnTo = "";
     res.redirect(newPath);
-  }else {
+  } else {
     res.redirect("/profile");
   }
 });

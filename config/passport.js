17e5c6c2be45142b878cea4c19884837a4b6b735
cgit6 <cgit67534@gmail.com>
2022-08-27 03:06:58 +0800

    d commit

--- config/passport.js
+++ config/passport.js
@@ -1,101 +1,74 @@
-// 這個passport.js 檔案是做什麼的?
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const User = require("../models/user-model");
 const LocalStrategy = require("passport-local");
-const bcrypt =require("bcrypt");
+const bcrypt = require("bcrypt");
 
-// req.user
-// req.logout
-// req.isAuthenticated()
-
-
-passport.serializeUser((user, done) =>{
-  // console.log("Serializing user now");
-  done(null,user._id);
+passport.serializeUser((user, done) => {
+  console.log("Serializing user now");
+  done(null, user._id);
 });
-// cookies 會跟著req 送到server來
-// server 需要去deserializeUser
-passport.deserializeUser(( _id,done) => {
-  // console.log("deserializeUser now");
-  User.findById({_id}).then((user) =>{
-    console.log("found user");
+
+passport.deserializeUser((_id, done) => {
+  console.log("Deserializing user now");
+  User.findById({ _id }).then((user) => {
+    console.log("Found user.");
     done(null, user);
   });
 });
-// 本地端登入驗證
+
 passport.use(
-  new LocalStrategy(
-    (username,password,done) => {
-      // console.log(username,password);
-      User.findOne({email: username}).then(
-        async user => {
-        // 如果使用者不存在
-        if(!user) {
-          return done(null,false);
+  new LocalStrategy((username, password, done) => {
+    console.log(username, password);
+    User.findOne({ email: username })
+      .then(async (user) => {
+        if (!user) {
+          return done(null, false);
         }
-        // 如果使用者存在
-        await bcrypt.compare(password,user.password ,function(err,result) {
+        await bcrypt.compare(password, user.password, function (err, result) {
           if (err) {
-            return done(null,false);
+            return done(null, false);
           }
           if (!result) {
-            return done(null,false);
+            return done(null, false);
           } else {
-            return done(null,user);
+            return done(null, user);
           }
         });
       })
       .catch((err) => {
-        return done(null,false);
-      })
-    }
-  )
+        return done(null, false);
+      });
+  })
+);
 
-)
-
-// 用於google api登入該系統
 passport.use(
   new GoogleStrategy(
     {
-    // 送到 .env 去
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/redirect",
-    
-    }, 
-  // passport callback
-  (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
-    User.findOne({googleID:profile.id}).then((foundUser) => {
-      // 如果資料庫裏面有這筆資料就是true會回傳"User already exist"
-      if(foundUser) {
-      
-        console.log("User already exist");
-        done(null, foundUser);
-
-      }
-      // 如果數據庫裏面沒有這筆資料就自己創造這筆資料並放進mongoDB裡面
-      // new... 一個obj
-      // save().then() Syntax
-      else {
-        // 建立
-        new User({
-          name: profile.displayName,
-          googleID: profile.id,
-          thumbnail: profile.photos[0].value,
-          email:profile.emails[0].value,
-
-        })
-        // 儲存
-        .save()
-        .then((newUser) => {
-          console.log("New user created.");
-          done(null, newUser);
-        });
-      }
-    })
-  })
-  );
-
-  module.exports = passport;
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/redirect",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+      User.findOne({ googleID: profile.id }).then((foundUser) => {
+        if (foundUser) {
+          console.log("User already exist");
+          done(null, foundUser);
+        } else {
+          new User({
+            name: profile.displayName,
+            googleID: profile.id,
+            thumbnail: profile.photos[0].value,
+            email: profile.emails[0].value,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("New user created.");
+              done(null, newUser);
+            });
+        }
+      });
+    }
+  )
+);
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -3,17 +3,14 @@ const passport = require("passport");
 const bcrypt = require("bcrypt");
 const User = require("../models/user-model");
 
-// 登入
 router.get("/login", (req, res) => {
   res.render("login", { user: req.user });
 });
 
-// 本地註冊
 router.get("/signup", (req, res) => {
   res.render("signup", { user: req.user });
 });
 
-// 登出
 router.get("/logout", (req, res) => {
   req.logOut();
   res.redirect("/");
@@ -23,13 +20,13 @@ router.post(
   "/login",
   passport.authenticate("local", {
     failureRedirect: "/auth/login",
-    failureFlash: "wrong email or password",
+    failureFlash: "Wrong email or password.",
   }),
   (req, res) => {
     if (req.session.returnTo) {
       let newPath = req.session.returnTo;
       req.session.returnTo = "";
-      res.redirect(newPath); // profile/path
+      res.redirect(newPath);
     } else {
       res.redirect("/profile");
     }
@@ -38,23 +35,20 @@ router.post(
 
 router.post("/signup", async (req, res) => {
   console.log(req.body);
-  // res.send("Thank for posting.")
   let { name, email, password } = req.body;
-  // check if the data is already in db
+  //check if the data is already in db
   const emailExist = await User.findOne({ email });
   if (emailExist) {
-    req.flash("error_msg", "Email has already been registered");
-    // redirect means?
+    req.flash("error_msg", "Email has already been registered.");
     res.redirect("/auth/signup");
   }
 
-  // 加密
   const hash = await bcrypt.hash(password, 10);
   password = hash;
   let newUser = new User({ name, email, password });
   try {
     await newUser.save();
-    req.flash("success_msg", "Registration succeeds. you can login now.");
+    req.flash("success_msg", "Registration succeeds. You can login now.");
     res.redirect("/auth/login");
   } catch (err) {
     req.flash("error_msg", err.errors.name.properties.message);
@@ -62,18 +56,10 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// 處理跟google authentication 有關的東西
 router.get(
   "/google",
-  // 要用passport 對google 做驗證
   passport.authenticate("google", {
-    // 什麼是 scope
-    // 什麼是 profile? 就是使用者在google的全部資料
-    // 什麼是 email 就是使用者在google的email資料
-
-    // 從google 驗證了使用者之後，server 想獲得使用者在google的資料
     scope: ["profile", "email"],
-    prompt: "select_account",
   })
 );
 
